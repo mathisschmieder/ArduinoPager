3db06236971182b0875bb4293b889ccc84d8a902
phl0 <florian@florian-wolters.de>
2017-05-09 13:13:38 +0200

    Fix whitspace and braces ;-)

--- ArduinoPager.ino
+++ ArduinoPager.ino
@@ -5,7 +5,7 @@
 #define IS_RFM69HCW   true // set to 'true' if you are using an RFM69HCW module
 
 #define SERIAL_BAUD   38400
- 
+
 #define RFM69_CS      10
 #define RFM69_IRQ     2
 #define RFM69_IRQN    0  // Pin 2 is IRQ 0!
@@ -15,7 +15,7 @@
 #define STATUS_LED    7
 
 #define FLASH_DELAY   32000U
- 
+
 RFM69 radio = RFM69(RFM69_CS, RFM69_IRQ, IS_RFM69HCW, RFM69_IRQN);
 
 String message = "";
@@ -27,7 +27,7 @@ uint32_t count = 0U;
 void setup() {
   while (!Serial); // wait until serial console is open, remove if not tethered to computer
   Serial.begin(SERIAL_BAUD);
-  
+
   // Hard Reset the RFM module
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, HIGH);
@@ -61,8 +61,7 @@ void loop() {
     led = !led;
     count = 0U;
   }
-  if (messageComplete) 
-  {
+  if (messageComplete) {
     char cmessage[512];
     digitalWrite(PTT_LED, HIGH);
     message.toCharArray(cmessage,message.length() + 1);
@@ -76,12 +75,10 @@ void loop() {
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    if (inChar != 0x17) // end of transmission
-    {
+    if (inChar != 0x17) { // end of transmission
       message += (inChar ^= 0xFF); // RFM69 needs the data inverted, hence XOR 0xFF
     }
-    else
-    {
+    else {
       messageComplete = true;
     }
   }
